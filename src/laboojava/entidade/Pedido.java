00e25f25f9ed7664c736527a48797230bc248e2a
Neo40 <tron36.cc@gmail.com>
2024-09-19 22:19:47 -0300

    Adição de entidades

---
+++ src/laboojava/entidade/Pedido.java
@@ -0,0 +1,14 @@
+package laboojava.entidade;
+
+/**
+ * Classe que representa a entidade pedido, qual é a compra dos produtos por um cliente.
+ */
+public class Pedido {
+
+    //TODO Preencher esta classe
+
+    //codigo
+    //cliente
+    //produtos
+    //total
+}
